b480313073abf158515d680629cd1688f4c997a3
Afeafa <estherkwashie7@gmail.com>
2016-03-17 06:10:58 -1200

    guesses a dice number

---
+++ Dice.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package assignment2_10531183;
+
+import java.util.Scanner;
+import java.util.Random;
+
+/**
+ *
+ * @author estherk
+ */
+public class Dice 
+{
+    public static void main(String[] args)
+    {
+        Random r = new Random();
+        int num1 = 1 + r.nextInt(6);
+        int num2 = 1 + r.nextInt(6);
+        
+        System.out.println("Roll #1: " + num1);
+        System.out.println("Roll #2: " + num2);
+        int result = num1 + num2;
+        System.out.println("the total is " + result );
+            
+    }
+    
+}
