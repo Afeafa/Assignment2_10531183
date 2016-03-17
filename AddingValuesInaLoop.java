af040394095ad34ae5fa00bbe1a25009ecdd0b7d
Afeafa <estherkwashie7@gmail.com>
2016-03-17 06:04:45 -1200

    this program adds values

---
+++ AddingValuesInaLoop.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package assignment2_10531183;
+
+/**
+ *
+ * @author estherk
+ */
+public class AddingValuesInaLoop 
+{
+    public static void main(String[] args)
+    {
+        
+    }
+    
+}
